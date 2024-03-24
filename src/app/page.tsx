14d56918f7a66dcfcf1bb44cc4c7ee9d319fcf62
Reuben Bishop <reubenjamesbishop@gmail.com>
2024-03-24 22:56:15 +1030

    fixed build issues and added landing scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { FaqSimple } from "./landing/components/FaqSimple";
+import { FeaturesGrid } from "./landing/components/FeaturesGrid";
+import { FooterLinks } from "./landing/components/FooterLinks";
+import { GetInTouchSimple } from "./landing/components/GetInTouchSimple";
 import { HeaderMegaMenu } from "./landing/components/HeaderMegaMenu";
 import { HeroBullets } from "./landing/components/HeroBullets";
 
@@ -8,6 +14,10 @@ export default function Home() {
     <>
       <HeaderMegaMenu />
       <HeroBullets />
+      <FeaturesGrid />
+      <FaqSimple />
+      <GetInTouchSimple />
+      <FooterLinks />
     </>
   );
 }
